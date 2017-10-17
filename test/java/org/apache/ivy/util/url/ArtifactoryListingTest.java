e106f1fbb0afc613150910d0cc0071baa6924aa8
Eugene Yokota <eed3si9n@gmail.com>
2017-10-16 23:31:03 -0500

    Update ArtifactoryListingTest
    http://repo.jfrog.org/artifactory/libs-releases-local/org/apache/wicket/wicket/ currently lists 3 items.

--- test/java/org/apache/ivy/util/url/ArtifactoryListingTest.java
+++ test/java/org/apache/ivy/util/url/ArtifactoryListingTest.java
@@ -30,6 +30,6 @@ public class ArtifactoryListingTest extends TestCase {
         
         List content = lister.listAll(new URL("http://repo.jfrog.org/artifactory/libs-releases-local/org/apache/wicket/wicket/"));
         assertNotNull(content);
-        assertEquals(5, content.size());
+        assertEquals(3, content.size());
     }
 }
